4bdaa506279660ed245d6c4670cb186653f4e07d
Telem Tobi <51072238+TelemTobi@users.noreply.github.com>
2023-10-28 10:04:49 +0300

    Initial Commit

---
+++ SwiftUI-Viper-Template/SwiftUI_Viper_TemplateApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Viper_TemplateApp.swift
+//  SwiftUI-Viper-Template
+//
+//  Created by Telem Tobi on 28/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Viper_TemplateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
