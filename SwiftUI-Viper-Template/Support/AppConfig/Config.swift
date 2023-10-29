c441633cfc35f26888ff754b301d067dc3bb3593
Telem Tobi <51072238+TelemTobi@users.noreply.github.com>
2023-10-29 08:45:00 +0200

    Config files

--- SwiftUI-Viper-Template/Support/AppConfig/Config.swift
+++ SwiftUI-Viper-Template/Support/AppConfig/Config.swift
@@ -9,7 +9,23 @@ import Foundation
 
 enum Config {
     
+    enum Key: String {
+        case someApiBaseurl
+    }
+    
+    static func value<T>(for key: Key) -> T? {
+        let value = Bundle.main.object(forInfoDictionaryKey: key.rawValue)
+        
+        switch T.self {
+        case is String.Type: return value as? T
+        case is Bool.Type: return (value as? String)?.boolValue as? T
+        default: return nil
+        }
+    }
+    
     enum someApi {
-        static let baseUrl = ""
+        static var baseUrl: String {
+            value(for: .someApiBaseurl) ?? ""
+        }
     }
 }
